8916292049898023d09a3e701fa19cd772437a3a
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2024-09-25 20:49:05 +0100

    Update layers_and_styles.py

--- layers_and_styles.py
+++ layers_and_styles.py
@@ -81,7 +81,7 @@ def get_styles():
         "streets": {
             "fc": "#adadad",  # Light grey streets fill
             "ec": "#5c5c5c",  # Medium-light grey for streets edge
-            "alpha": 0.5,
+            "alpha": 0.65,
             "lw": 0.3,        # Line width for streets
             "zorder": 3       # Layer order
         },
