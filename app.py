95072cdfdde0158e3939c396bb1b0e25e331abaa
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2024-09-25 21:32:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def create_prettymap_app():
         col1, col2 = st.columns(2)
 
         with col1:
-            png_data = save_plot_to_bytes(st.session_state["plot_fig"], file_format="png", transparent=True)
+            png_data = save_plot_to_bytes(st.session_state["plot_fig"], file_format="png")
             st.download_button(
                 label="Download as PNG",
                 data=png_data,
